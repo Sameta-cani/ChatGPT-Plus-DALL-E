3e1b5dbe4ed123a3307343db24d4b5cbcda87064
ParkSangJin <ticani0610@gmail.com>
2023-11-06 02:42:28 +0900

    req image upload test6

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ if img_file is not None:
 
       st.image(f'image/{img_file.name}')
 
-print(model.img2img)
+st.write(model.img2img)
 
 with st.form(key="form"):
     user_input = st.text_input(label="Prompt")
